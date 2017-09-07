78b4690168881674654c755c7163b2aa10d6afa4
b5710451592 <31470087+b5710451592@users.noreply.github.com>
2017-09-07 22:48:19 +0700

    calendar
    calendar_ver2 5710451592

---
+++ MySchedule_ver2/src/main/java/View.java
@@ -0,0 +1,92 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import org.sqlite.SQLiteException;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+/**
+ *
+ */
+public class View  {
+
+
+
+    @FXML
+    protected ListView listView= new ListView();
+
+    @FXML
+    protected ObservableList<String>items = FXCollections.observableArrayList ();
+            //"Single", "Double", "Suite", "Family App");
+
+    @FXML
+    protected Button addButton;
+
+    @FXML
+    protected Button removeButton;
+
+    @FXML
+    protected TextArea textAreaDetail;
+
+    @FXML
+    protected TextField textFieldDate;
+
+    @FXML
+    protected  TextField textFieldTime;
+
+    @FXML
+    protected TextField textFieldRemove;
+    @FXML
+    protected TableView tableViewMyschedule;
+    Controller controller = new Controller();
+
+
+
+
+    @FXML
+    public void ActionButtomAdd(ActionEvent event)throws SQLException{
+
+        String t=textFieldDate.getText()+" | "+textFieldTime.getText()+" | "+textAreaDetail.getText(); // get by textfield not DB
+        items.add(t);
+
+        try {
+            controller.insert(textFieldDate.getText(), textFieldTime.getText(), textAreaDetail.getText());
+            controller.loadData(items,listView);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage()+" ActionButtomAdd");
+        }
+
+
+        textAreaDetail.clear();
+        textFieldDate.clear();
+        textFieldTime.clear();
+    }
+
+    @FXML
+    public void actionButtonRemove(ActionEvent event)throws SQLException{
+
+        try {
+
+            controller.remove(textFieldRemove.getText());
+
+            controller.loadData(items,listView);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage()+" actionButtonRemove");
+        }
+
+    }
+
+
+
+
+
+
+
+}
